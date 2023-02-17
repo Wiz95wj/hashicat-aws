6269113ae9ea597facf789ef3b2bf11d44dfab4f
Wei Jie <weijiedsta@gmail.com>
2023-02-17 06:21:07 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -20,22 +20,3 @@ module "s3_bucket_for_logs" {
 
   attach_elb_log_delivery_policy = true
 }
-module "s3_bucket_for_logs" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket-for-logs"
-  acl    = "log-delivery-write"
-
-  # Allow deletion of non-empty bucket
-  force_destroy = true
-
-  attach_elb_log_delivery_policy = true  # Required for ALB logs
-  attach_lb_log_delivery_policy  = true  # Required for ALB/NLB logs
-}
-# This S3 bucket will not be created
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  create_bucket = false
-  # ... omitted
-}
