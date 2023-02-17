b5f282875e311798dda078d72e12df6e3eebbb36
Wei Jie <weijiedsta@gmail.com>
2023-02-17 06:30:30 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,22 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
-}
-module "s3_bucket_for_logs" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket-for-logs"
-  acl    = "log-delivery-write"
-
-  # Allow deletion of non-empty bucket
-  force_destroy = true
-
-  attach_elb_log_delivery_policy = true
+module "s3-bucket" {
+  source  = "app.terraform.io/11nnovate-wj/s3-bucket/aws"
+  version = "2.8.0"
 }
